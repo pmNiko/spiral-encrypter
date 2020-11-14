3efc9f4905aaafbbb7eaa2ff0c9e4cdb4cb67197
pmNiko <nikolas090189@gmail.com>
2020-11-14 00:39:53 -0300

    Add alphabet to quadrant

--- main.py
+++ main.py
@@ -6,24 +6,35 @@ print("FLOGRAMA HASH")
 # print("\n")
 # print("Bienvenido ", name)
 
-# a = Alphabet()
-# a.position()
+alf = Alphabet()
+# vector = alf.data
 
-a = [1,2,3,4]
-c = Quadrant(4,'Primer Cuadrante')
-print(c.items)
+cuadrante_1 = Quadrant(1,0,'Primer Cuadrante')
+cuadrante_2 = Quadrant(1,1,'Segundo Cuadrante')
+cuadrante_3 = Quadrant(2,2,'Tercero Cuadrante')
+cuadrante_4 = Quadrant(3,4,'Cuarto Cuadrante')
+cuadrante_5 = Quadrant(5,7,'Quinto Cuadrante')
+cuadrante_6 = Quadrant(8,12,'Sexto Cuadrante')
+cuadrante_7 = Quadrant(13,20,'Septimo Cuadrante')
+cuadrante_8 = Quadrant(21,33,'Octavo Cuadrante')
 
-c.add(a)
-print(c.items, c.name)
+cuadrante_1.add(alf.data)
+cuadrante_2.add(alf.data)
+cuadrante_3.add(alf.data)
+cuadrante_4.add(alf.data)
+cuadrante_5.add(alf.data)
+cuadrante_6.add(alf.data)
+cuadrante_7.add(alf.data)
+cuadrante_8.add(alf.data)
 
-# def bla(parameter_list):
-#     b = []
-    
-#     for ele in parameter_list:
-#         b.append(ele)
-    
-#     print(b)
+print(cuadrante_1.name, cuadrante_1.items)
+print(cuadrante_2.name, cuadrante_2.items)
+print(cuadrante_3.name, cuadrante_3.items)
+print(cuadrante_4.name, cuadrante_4.items)
+print(cuadrante_5.name, cuadrante_5.items)
+print(cuadrante_6.name, cuadrante_6.items)
+print(cuadrante_7.name, cuadrante_7.items)
+print(cuadrante_8.name, cuadrante_8.items)
 
-
-# bla(a)
-
+# for x in range(5,len(alf.data)):
+#     print(alf.data[x])
--- quadrant.py
+++ quadrant.py
@@ -1,13 +1,11 @@
 class Quadrant:
-    def __init__(self, number, name):
+    def __init__(self, large, start,name):
         self.items = []
-        self.large = number
+        self.large = large
+        self.start = start
         self.name = name
+
     def add(self,parameter_list):
-        for ele in parameter_list:
-            self.items.append(ele)
-    
-    # def procesar(characters):
-    #     if len(self.tems) <= self.large:
-    #     for char in characters
-    #         sel
+        for ele in range(self.start,len(parameter_list)):
+            if len(self.items) < self.large:
+                self.items.append(parameter_list[ele])
