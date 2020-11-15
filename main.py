126f7dbfd260aad48b14bb33d61f0123346bb06c
pmNiko <nikolas090189@gmail.com>
2020-11-15 19:00:25 -0300

    Add MENU

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from encrypter import Encrypter
 
-print("Spiral Encrypter")
-
 encripter = Encrypter()
 
-speed = int(input("Indique frecuencia: "))
-encripter.spinner(speed)
+print("SPIRAL-ECRIPTER")
 
-chars = input("Escriba su frase: ")
-encripter.encrypt(chars)
+option = 0
+while option < 3:
+    option = int(input("\nElija una opción => 1:Encriptar -  2:Desencriptar - 3:Finalizar :"))
+    if option == 1:
+        print('\nECRIPTADO')
+        speed = int(input("Indique la frecuencia: "))
+        encripter.spinner(speed)
+        chars = input("Escriba su frase: ")
+        encripter.encrypt(chars)
+        option = 1
 
-hash_encrypt = input("\nDesencripte su hash: ")
-encripter.decrypt(hash_encrypt)
+    if option == 2:
+        print('\nDESENCRIPTADO')
+        speed = int(input("Indique la frecuencia: "))
+        encripter.spinner(speed)
+        hash_encrypt = input("Ingrese su hash: ")
+        encripter.decrypt(hash_encrypt)
+        option = 2
+
+    if option == 3:
+        print('\n------ Fin del programa -------\n')    
+        option = 3
 
 
 
